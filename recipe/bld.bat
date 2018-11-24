021368cbae89de5ac8dbbaf90fee440bdfcfc924
Isuru Fernando <isuruf@gmail.com>
2018-11-24 03:20:05 -0600

    Fix mingw32-make location

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ REM Trick to avoid CMake/sh.exe error
 ren "C:\Program Files\Git\usr\bin\sh.exe" _sh.exe
 
 cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DBUILD_SHARED_LIBS=yes -DCMAKE_BUILD_TYPE=Release -DLAPACKE=ON -DCBLAS=ON -Wno-dev ..
-%LIBRARY_PREFIX%\mingw-w64\bin\mingw32-make
-%LIBRARY_PREFIX%\mingw-w64\bin\mingw32-make install
+mingw32-make -j%CPU_COUNT%
+mingw32-make install
 
 ctest --output-on-failure
