76955d139c071f261b1b5d736584a5add17317ce
Isuru Fernando <isuruf@gmail.com>
2018-11-24 02:56:13 -0600

    Skip failing tests on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,7 +15,17 @@ cmake \
   ..
 
 make -j${CPU_COUNT}
-ctest --output-on-failure
+
+if [[ $(uname) == "Darwin" ]]; then
+  # testing with shared libraries does not work. skip them
+  # to test that program exits if wrong parameters are given, what the testsuite
+  # do is that the symbol xerbla (xerbla logs the error and exits) is overriden
+  # by the test program's own version which reports to the test program that
+  # xerbla was called. This does not work with dylibs on osx and dlls on windows
+  ctest --output-on-failure -E "x*cblat2|x*cblat3"
+else
+  ctest --output-on-failure
+fi
 make install
 
 
