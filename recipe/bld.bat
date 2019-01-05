eec8444f015df6d69ecc683c661720389b8b21a1
Isuru Fernando <isuruf@gmail.com>
2019-01-04 20:30:18 -0600

    Use flang

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,17 @@
 mkdir build
 cd build
 
-REM Trick to avoid CMake/sh.exe error
-ren "C:\Program Files\Git\usr\bin\sh.exe" _sh.exe
+cmake -G "Ninja" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_C_COMPILER=clang-cl ^
+    -DCMAKE_Fortran_COMPILER=flang ^
+    -DBUILD_SHARED_LIBS=yes ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DLAPACKE=ON ^
+    -DCBLAS=ON ^
+    -Wno-dev ..
 
-cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DBUILD_SHARED_LIBS=yes -DCMAKE_BUILD_TYPE=Release -DLAPACKE=ON -DCBLAS=ON -Wno-dev ..
-mingw32-make -j%CPU_COUNT%
-mingw32-make install
+ninja -j%CPU_COUNT%
+ninja install
 
 ctest --output-on-failure
