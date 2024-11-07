a05496dbe0d3080c95199b177f3bb57c908bfbba
H. Vetinari <h.vetinari@gmx.com>
2024-11-08 06:34:27 +1100

    Keep library naming as before
    This partially reverts commit ebff8dac10adb2f01d62bbd9b2a56bb341aae168.
    
    don't patch in OUTPUT_NAME; backport patch for using CMAKE_SHARED_LIBRARY_PREFIX

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,7 @@ cmake -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DBUILD_SHARED_LIBS=yes ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_SHARED_LIBRARY_PREFIX=lib ^
     -DBUILD_TESTING=ON ^
     -DLAPACKE=ON ^
     -DCBLAS=ON ^
@@ -16,6 +17,13 @@ cmake -G "Ninja" ^
 ninja -j%CPU_COUNT%
 ninja install
 if %ERRORLEVEL% NEQ 0 exit 1
+
+:: strip "lib" prefix from import libraries, in line with previous naming
+move %LIBRARY_LIB%\libblas.lib %LIBRARY_LIB%\blas.lib
+move %LIBRARY_LIB%\libcblas.lib %LIBRARY_LIB%\cblas.lib
+move %LIBRARY_LIB%\liblapack.lib %LIBRARY_LIB%\lapack.lib
+move %LIBRARY_LIB%\liblapacke.lib %LIBRARY_LIB%\lapacke.lib
+move %LIBRARY_LIB%\libtmglib.lib %LIBRARY_LIB%\tmglib.lib
 
 :: testing with shared libraries does not work - skip them.
 :: This is because: to test that the program exits if wrong parameters are given,
